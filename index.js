a1df8fe6d5ca16578543ef7b70a8ce420959c67d
BB-Code <15919854639@163.com>
2023-03-16 17:05:16 +0800

    feat: add upload file server

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const express = require('express')
+const express = require('express');
+const multer = require('multer');
+var fs = require("fs");
+const upload = multer({ dest: 'upload/' });
 const app = express();
 const port = 9000
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    //res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
+app.post('/profile', upload.single('fileHandler'), function (req, res, next) {
+    console.log(req.file)
+    let savePath = __dirname + "/upload/" + req.file.originalname;
+    let fileData = fs.readFileSync(req.file.path);
+    fs.writeFile(savePath, fileData, (err, data) => {
+        if (err) res.send(err);
+        let response = {
+            message: 'File uploaded successfully',
+            filename: req.file.originalname
+        };
+        res.end(JSON.stringify(response));
+    });
+})
 app.use(express.static('public'))
-app.get(`/`,(req,res)=>{
+app.get(`/`, (req, res) => {
     res.send(`This is file server!`)
 });
 
-app.listen(port,()=>{
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", 'express')
+    //res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+
+app.listen(port, () => {
     console.log(`file server is listening on port ${port}`)
 })
